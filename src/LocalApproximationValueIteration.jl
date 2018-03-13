b7df65bae1fb00049ab905b08f013de990f4e0e6
Shushman Choudhury <shushmanchoudhury@gmail.com>
2018-03-13 12:24:37 -0700

    Basic module copied from DiscreteVI

--- src/LocalApproximationValueIteration.jl
+++ src/LocalApproximationValueIteration.jl
@@ -1,5 +1,21 @@
+"""
+This module implements Value Iteration for large/continuous state spaces by solving for some 
+subspace of the state space and interpolating the value function over the rest of the state space
+"""
 module LocalApproximationValueIteration
 
-# package code goes here
+using POMDPs
+using POMDPToolbox
+
+import POMDPs: Solver, solve, Policy, action, value 
+
+export
+    LocalApproxValueIterationPolicy,
+    LocalApproxValueIterationSolver,
+    create_policy,
+    solve,
+    action,
+    value,
+    locals
 
 end # module
