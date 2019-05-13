973b5ba078875c11bd01929591a29147446bd71d
Shushman <shushmanchoudhury@gmail.com>
2019-05-13 11:51:21 -0700

    Reworked tests significantly

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,5 +9,13 @@ using LocalFunctionApproximation
 using LocalApproximationValueIteration
 using Test
 
-# write your own tests here
-include("runtests_versus_discrete_vi.jl")
+@testset "all" begin
+
+    println("Testing Requirements")
+    gifa = LocalGIFunctionApproximator(RectangleGrid([0.0, 1.0], [0.0, 1.0]))
+    @requirements_info LocalApproximationValueIterationSolver(gifa) SimpleGridWorld()
+
+    @testset "integration" begin
+        include("runtests_versus_discrete_vi.jl")
+    end
+end
